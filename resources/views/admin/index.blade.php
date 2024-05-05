520c3fb8b14c9d5d5701306c752a29c1ad5804b0
Shoaib Tariq <shoaibtariq554@gmail.com>
2024-05-05 15:14:22 +0500

    add card on dashboard

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,15 +1,62 @@
 @extends('layouts.admin.app')
 
 @section('content')
-<div class="container-fluid">
-    <div class="row">
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-body">
-                    <h3>Welcome to admin panel</h3>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-xl-12">
+                {{-- <div class="card">
+                <div class="card-body"> --}}
+                <div class="row separate-row">
+                    <div class="col-sm-4">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="job-icon pb-4 d-flex justify-content-between">
+                                    <div>
+                                        <div class="d-flex align-items-center mb-1">
+                                            <h2 class="mb-0 lh-1">4</h2>
+                                        </div>
+                                        <span class="fs-14 d-block mb-2">Total Cars</span>
+                                    </div>
+                                    <div id="NewCustomers"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-4">
+
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="job-icon pb-4 pt-4 pt-sm-0 d-flex justify-content-between">
+                                    <div>
+                                        <div class="d-flex align-items-center mb-1">
+                                            <h2 class="mb-0 lh-1">4</h2>
+                                        </div>
+                                        <span class="fs-14 d-block mb-2">Total Users</span>
+                                    </div>
+                                    <div id="NewCustomers1"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-4">
+
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="job-icon pt-4 pb-sm-0 pb-4 pe-3 d-flex justify-content-between">
+                                    <div>
+                                        <div class="d-flex align-items-center mb-1">
+                                            <h2 class="mb-0 lh-1">100</h2>
+                                        </div>
+                                        <span class="fs-14 d-block mb-2">Total Booking</span>
+                                    </div>
+                                    <div id="NewCustomers2"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
