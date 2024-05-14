486118719f29505c7519e425d42e8cf780d853c6
Shoaib Tariq <shoaibtariq554@gmail.com>
2024-05-15 03:54:17 +0500

    updates

--- resources/views/frontend/services.blade.php
+++ resources/views/frontend/services.blade.php
@@ -17,59 +17,68 @@
 <div class="line-vr-section"></div>
 <!-- Services Box 2 -->
 
-<section class="testimonials ">
+<section class="testimonials section-padding mt-15">
     <div class="container">
         <div class="row">
             <div class="col-md-12 text-center mb-30">
                 <div class="section-subtitle"></div>
-                    <h1 class="section-title">Our <span>Services</span></h1>
-                
+                <h1 class="section-title">Our <span>Services</span></h1>
+
             </div>
             <div class="col-md-12">
                 <div class="owl-carousel owl-theme">
                     <div class="item" style="padding:10px;">
                         <div class="cars_details_view">
                             <div>
-                                 <img src="{{ asset('frontend-assets/img/slider/5.jpg') }}" alt="" />
+                                <img src="{{ asset('frontend-assets/img/slider/5.jpg') }}" alt="" />
                                 <h4 class="text-white mb-0 pt-2">BMW 3-Series</h4>
                                 <p style="color: #f5b754">Moderline</p>
                                 <p style="color: white ;" class="truncate">
-                                    Quisque pretium fermentum quam, sit amet cursus ante sollicitudin vel. Morbi consequat risus consequat, porttitor orci sit amet, iaculis nisl. Integer quis sapien nec elit ultrices euismon sit amet id lacus. Sed a imperdiet erat. </p>
-                                <div class="">
-                                    <a class="view_details" href="{{ route('frontend.carDetails') }}">View Details</a>
-                                </div>
+                                    Quisque pretium fermentum quam, sit amet cursus ante sollicitudin vel. Morbi
+                                    consequat risus consequat, porttitor orci sit amet, iaculis nisl. Integer quis
+                                    sapien nec elit ultrices euismon sit amet id lacus. Sed a imperdiet erat. </p>
+                                    <div class="d-flex justify-content-between gap-4">
+                                        <a class="view_details" href="{{ route('frontend.carDetails') }}">View Details</a>
+                                        <a class="view_details" href="{{ route('frontend.book-online') }}">Book Now</a>
+                                    </div>
                             </div>
-                            
+
                         </div>
                     </div>
                     <div class="item" style="padding:10px;">
                         <div class="cars_details_view">
                             <div>
-                                 <img src="{{ asset('frontend-assets/img/slider/11.jpg') }}" alt="" />
+                                <img src="{{ asset('frontend-assets/img/slider/11.jpg') }}" alt="" />
                                 <h4 class="text-white mb-0 pt-2">BMW 3-Series</h4>
                                 <p style="color: #f5b754">Moderline</p>
-                                <p style="color: white ;" class="truncate" >
-                                    Quisque pretium fermentum quam, sit amet cursus ante sollicitudin vel. Morbi consequat risus consequat, porttitor orci sit amet, iaculis nisl. Integer quis sapien nec elit ultrices euismon sit amet id lacus. Sed a imperdiet erat. </p>
-                                <div class="">
-                                    <a class="view_details" href="{{ route('frontend.carDetails') }}">View Details</a>
-                                </div>
+                                <p style="color: white ;" class="truncate">
+                                    Quisque pretium fermentum quam, sit amet cursus ante sollicitudin vel. Morbi
+                                    consequat risus consequat, porttitor orci sit amet, iaculis nisl. Integer quis
+                                    sapien nec elit ultrices euismon sit amet id lacus. Sed a imperdiet erat. </p>
+                                    <div class="d-flex justify-content-between gap-4">
+                                        <a class="view_details" href="{{ route('frontend.carDetails') }}">View Details</a>
+                                        <a class="view_details" href="{{ route('frontend.book-online') }}">Book Now</a>
+                                    </div>
                             </div>
-                            
+
                         </div>
                     </div>
                     <div class="item" style="padding:10px;">
                         <div class="cars_details_view">
                             <div>
-                                 <img src="{{ asset('frontend-assets/img/slider/9.jpg') }}" alt="" />
+                                <img src="{{ asset('frontend-assets/img/slider/9.jpg') }}" alt="" />
                                 <h4 class="text-white mb-0 pt-2">BMW 3-Series</h4>
                                 <p style="color: #f5b754">Moderline</p>
-                                <p style="color: white"  class="truncate" >
-                                    Quisque pretium fermentum quam, sit amet cursus ante sollicitudin vel. Morbi consequat risus consequat, porttitor orci sit amet, iaculis nisl. Integer quis sapien nec elit ultrices euismon sit amet id lacus. Sed a imperdiet erat. </p>
-                                <div class="">
-                                    <a class="view_details" href="{{ route('frontend.carDetails') }}">View Details</a>
-                                </div>
+                                <p style="color: white" class="truncate">
+                                    Quisque pretium fermentum quam, sit amet cursus ante sollicitudin vel. Morbi
+                                    consequat risus consequat, porttitor orci sit amet, iaculis nisl. Integer quis
+                                    sapien nec elit ultrices euismon sit amet id lacus. Sed a imperdiet erat. </p>
+                                    <div class="d-flex justify-content-between gap-4">
+                                        <a class="view_details" href="{{ route('frontend.carDetails') }}">View Details</a>
+                                        <a class="view_details" href="{{ route('frontend.book-online') }}">Book Now</a>
+                                    </div>
                             </div>
-                            
+
                         </div>
                     </div>
 
