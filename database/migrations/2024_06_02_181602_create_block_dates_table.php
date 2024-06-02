2e6a4dd4ce2f92f26805f0f8ed3817ba04cffa08
Muzammil <muzammilshahzad894@gmail.com>
2024-06-02 23:26:07 +0500

    add migratinos

---
+++ database/migrations/2024_06_02_181602_create_block_dates_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBlockDatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('block_dates', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->string('date_range')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('block_dates');
+    }
+}
---
+++ database/migrations/2024_06_02_182101_create_bookings_table.php
@@ -0,0 +1,56 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBookingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->string('email')->nullable();
+            $table->string('phone_number')->nullable();
+            $table->string('other_name')->nullable();
+            $table->string('other_email')->nullable();
+            $table->string('other_phone_number')->nullable();
+            $table->integer('is_childseat')->nullable();
+            $table->integer('is_meet_nd_greet')->nullable();
+            $table->string('pickup_location')->nullable();
+            $table->string('dropoff_location')->nullable();
+            $table->string('booking_date')->nullable();
+            $table->string('booking_time')->nullable();
+            $table->string('fleet_id')->nullable();
+            $table->string('service_id')->nullable();
+            $table->integer('no_of_passenger');
+            $table->integer('no_suit_case')->nullable();
+            $table->text('summary')->nullable();
+            $table->integer('no_of_laugage')->nullable();
+            $table->string('flight_time')->nullable();
+            $table->string('flight_name')->nullable();
+            $table->string('flight_type')->nullable();
+            $table->integer('is_payment')->default(0);
+            $table->integer('is_draft')->default(1);
+            $table->integer('user_id')->nullable();
+            $table->string('total_price')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bookings');
+    }
+}
