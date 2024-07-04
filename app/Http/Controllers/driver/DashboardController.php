6b14d7ab7edd76ee52650356b982e9c483ca202b
Muzammil <muzammilshahzad894@gmail.com>
2024-07-04 19:04:59 +0500

    show booking details on driver dashboard

--- app/Http/Controllers/driver/DashboardController.php
+++ app/Http/Controllers/driver/DashboardController.php
@@ -8,6 +8,7 @@ use Exception;
 use Illuminate\Http\Request;
 use App\Http\Requests\UpdateProfileRequest;
 use App\Models\User;
+use App\Models\Booking;
 
 class DashboardController extends Controller
 {
@@ -24,7 +25,12 @@ class DashboardController extends Controller
     public function dashboard()
     {
         try {
-            return view('driver.index');
+            $todayPendingBookings = Booking::where('status_id', 1)->where('assigned_to', auth()->user()->id)->whereDate('booking_date', date('Y-m-d'))->count();
+            $todayInprogressBookings = Booking::whereIn('status_id', [2, 3, 4])->where('assigned_to', auth()->user()->id)->whereDate('booking_date', date('Y-m-d'))->count();
+            $todayCompletedBookings = Booking::where('status_id', 5)->where('assigned_to', auth()->user()->id)->whereDate('booking_date', date('Y-m-d'))->count();
+            $upcomingBookings = Booking::where('assigned_to', auth()->user()->id)->whereDate('booking_date', '>', date('Y-m-d'))->count();
+
+            return view('driver.index', compact('todayPendingBookings', 'todayInprogressBookings', 'todayCompletedBookings', 'upcomingBookings'));
         } catch (Exception $e) {
             Log::error(__CLASS__ . '::' . __LINE__ . ' Exception: ' . $e->getMessage());
             return redirect()->back()->with('error', 'Something went wrong');
--- resources/views/driver/index.blade.php
+++ resources/views/driver/index.blade.php
@@ -13,44 +13,56 @@
                                 <div class="job-icon pb-4 d-flex justify-content-between">
                                     <div>
                                         <div class="d-flex align-items-center mb-1">
-                                            <h2 class="mb-0 lh-1">4</h2>
+                                            <h2 class="mb-0 lh-1">{{ $todayPendingBookings }}</h2>
                                         </div>
-                                        <span class="fs-14 d-block mb-2">Total Bookings</span>
+                                        <span class="fs-14 d-block mb-2">Today Pending Bookings</span>
                                     </div>
-                                    <div id="NewCustomers"></div>
+                                    <i class="fa fa-car pending-bookings" aria-hidden="true"></i>
                                 </div>
                             </div>
                         </div>
                     </div>
-
                     <div class="col-sm-4">
-
                         <div class="card">
                             <div class="card-body">
                                 <div class="job-icon pb-4 pt-4 pt-sm-0 d-flex justify-content-between">
                                     <div>
                                         <div class="d-flex align-items-center mb-1">
-                                            <h2 class="mb-0 lh-1">4</h2>
+                                            <h2 class="mb-0 lh-1">{{ $todayInprogressBookings }}</h2>
                                         </div>
-                                        <span class="fs-14 d-block mb-2">Pending</span>
+                                        <span class="fs-14 d-block mb-2">Today Inprogress Bookings</span>
                                     </div>
-                                    <div id="NewCustomers1"></div>
+                                    <i class="fa fa-car inprogress-bookings" aria-hidden="true"></i>
                                 </div>
                             </div>
                         </div>
                     </div>
                     <div class="col-sm-4">
-
                         <div class="card">
                             <div class="card-body">
-                                <div class="job-icon pt-4 pb-sm-0 pb-4 pe-3 d-flex justify-content-between">
+                                <div class="job-icon pb-4 d-flex justify-content-between">
                                     <div>
                                         <div class="d-flex align-items-center mb-1">
-                                            <h2 class="mb-0 lh-1">100</h2>
+                                            <h2 class="mb-0 lh-1">{{ $todayCompletedBookings }}</h2>
                                         </div>
-                                        <span class="fs-14 d-block mb-2">Completed</span>
+                                        <span class="fs-14 d-block mb-2">Today Completed Rides</span>
                                     </div>
-                                    <div id="NewCustomers2"></div>
+                                    <i class="fa fa-car completed-bookings" aria-hidden="true"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-4">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="job-icon pb-4 d-flex justify-content-between">
+                                    <div>
+                                        <div class="d-flex align-items-center mb-1">
+                                            <h2 class="mb-0 lh-1">{{ $upcomingBookings }}</h2>
+                                        </div>
+                                        <span class="fs-14 d-block mb-2">Upcoming Bookings</span>
+                                    </div>
+                                    <i class="fa fa-car pending-bookings" aria-hidden="true"></i>
                                 </div>
                             </div>
                         </div>
@@ -59,4 +71,21 @@
             </div>
         </div>
     </div>
+@endsection
+
+@section('styles')
+    <style>
+        .pending-bookings {
+            color: #F93A0B;
+            font-size: xx-large;
+        }
+        .inprogress-bookings {
+            color: #3385D6;
+            font-size: xx-large;
+        }
+        .completed-bookings {
+            color: #2ECC71;
+            font-size: xx-large;
+        }
+    </style>
 @endsection
