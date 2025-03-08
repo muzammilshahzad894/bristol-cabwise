3082195adaa79c716d1d90373d4f75f7f56c4dc3
Muzammil <muzammilshahzad894@gmail.com>
2025-03-08 17:45:30 +0500

    remove required from email-content-setting form

--- resources/views/admin/email-content-settings/edit.blade.php
+++ resources/views/admin/email-content-settings/edit.blade.php
@@ -20,7 +20,7 @@
                                 </div>
                                 <div class="mb-3 col-md-6">
                                     <label class="form-label">Subject <span class="text-danger">*</span></label>
-                                    <input type="text" name="subject" class="form-control" placeholder="Subject" value="{{ old('subject') ? old('subject') : $emailContentSetting->subject }}" required>
+                                    <input type="text" name="subject" class="form-control" placeholder="Subject" value="{{ old('subject') ? old('subject') : $emailContentSetting->subject }}">
                                     @error('subject')
                                         <div class="text-danger">{{ $message }}</div>
                                     @enderror
@@ -29,21 +29,21 @@
                             <div class="row">
                                 <div class="mb-3 col-md-6">
                                     <label class="form-label">Introductory Message <span class="text-danger">*</span></label>
-                                    <textarea name="introductory_message" class="form-control mceEditor" rows="7" placeholder="Introductory Message" required>{{ old('introductory_message') ? old('introductory_message') : $emailContentSetting->introductory_message }}</textarea>
+                                    <textarea name="introductory_message" class="form-control mceEditor" rows="7" placeholder="Introductory Message">{{ old('introductory_message') ? old('introductory_message') : $emailContentSetting->introductory_message }}</textarea>
                                     @error('introductory_message')
                                         <div class="text-danger">{{ $message }}</div>
                                     @enderror
                                 </div>
                                 <div class="mb-3 col-md-6">
                                     <label class="form-label">Note <span class="text-danger">*</span></label>
-                                    <textarea name="note" class="form-control mceEditor" rows="7" placeholder="Note" required>{{ old('note') ? old('note') : $emailContentSetting->note }}</textarea>
+                                    <textarea name="note" class="form-control mceEditor" rows="7" placeholder="Note">{{ old('note') ? old('note') : $emailContentSetting->note }}</textarea>
                                     @error('note')
                                         <div class="text-danger">{{ $message }}</div>
                                     @enderror
                                 </div>
                                 <div class="mb-3 col-md-6">
                                     <label class="form-label">Closing Text <span class="text-danger">*</span></label>
-                                    <textarea name="closing_text" class="form-control mceEditor" rows="7" placeholder="Closing Text" required>{{ old('closing_text') ? old('closing_text') : $emailContentSetting->closing_text }}</textarea>
+                                    <textarea name="closing_text" class="form-control mceEditor" rows="7" placeholder="Closing Text">{{ old('closing_text') ? old('closing_text') : $emailContentSetting->closing_text }}</textarea>
                                     @error('closing_text')
                                         <div class="text-danger">{{ $message }}</div>
                                     @enderror
